9e504c2d026c570a6097162323d86ec77c21fcd4
Aijaz Ali Inayat <webextolcollege@gmail.com>
2024-12-31 01:31:44 +0500

    url(extra-images/bg-image1.jpg not loaded

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'foodonline_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'foodonline_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodonline_db',
+        'USER': 'postgres',
+        'PASSWORD': 'aijaz123',
     }
 }
 
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'foodonline_main/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- foodonline_main/views.py
+++ foodonline_main/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the foodonline_main index.")
+    return render(request, 'home.html')
